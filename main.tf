cfad601dfea11582f922c6127b44d2660590714a
kanobi <bulutovstas@mail.ru>
2022-02-02 15:00:20 +0300

    Created two VPS with nginx

--- main.tf
+++ main.tf
@@ -16,9 +16,13 @@ terraform {
       source  = "hashicorp/local"
       version = "2.1.0"
     }
-
+   null = {
+      source = "hashicorp/null"
+      version = "3.1.0"
+    }
   }
 }
+
 
 provider "digitalocean" {
   token = var.do_token
@@ -27,9 +31,11 @@ provider "digitalocean" {
 resource "digitalocean_tag" "ansible-01" {
   name = "bulutovstas_at_mail_ru"
 }
+
 resource "digitalocean_droplet" "ansible-01" {
+  count    = length(var.devs)
   image    = "ubuntu-18-04-x64"
-  name     = "ansible-task-01"
+  name     = "ubuntu-${format("%02d", count.index + 1)}"
   region   = "ams3"
   size     = "s-1vcpu-1gb"
   tags     = [digitalocean_tag.ansible-01.name]
@@ -43,20 +49,18 @@ provider "aws" {
 }
 
 resource "aws_route53_record" "domain" {
+  count   = length(var.devs)
   zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "bulutovstas.${data.aws_route53_zone.selected.name}"
+  name    = "${var.devs[count.index].login}-${var.devs[count.index].prefix}.${data.aws_route53_zone.selected.name}"
   type    = "A"
   ttl     = "300"
-  records = [local.ip_addr]
+  records = ["${element(digitalocean_droplet.ansible-01.*.ipv4_address, count.index)}"]
 }
 
 data "aws_route53_zone" "selected" {
   name = "devops.rebrain.srwx.net"
 }
 
-locals {
-  ip_addr = digitalocean_droplet.ansible-01.ipv4_address
-}
 
 data "digitalocean_ssh_key" "rebrain_key" {
   name = "REBRAIN.SSH.PUB.KEY"
@@ -71,7 +75,8 @@ data "template_file" "inventory_template" {
 
   template = file("${path.module}/inventory.tmpl")
   vars = {
-    domain = aws_route53_record.domain.name
+    balancer = aws_route53_record.domain[0].name
+    app = aws_route53_record.domain[1].name
   }
 
 }
@@ -79,4 +84,13 @@ data "template_file" "inventory_template" {
 resource "local_file" "inventory_file" {
   filename = "${path.module}/inventory"
   content  = data.template_file.inventory_template.rendered
+}
+
+resource "null_resource" "inventory_file" {
+ provisioner "local-exec" {
+    command = "ansible-playbook -i ${path.module}/inventory --private-key ${var.private_ssh_key} ${path.module}/ansible_playbook.yml -b"
+}
+depends_on = [
+  local_file.inventory_file,
+]
 }
--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "do_token" {
 
 variable "ssh_key" {
   type        = string
-  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBVki2X88HDd+iq3LZIdt4RN4xYfOhOJxYgV/7KtI/p5"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9EFrctUw5Oq1oaEvSl5halIbR7K9ISxOUQ6uVA67/s2yEDO5wUetQTr8gq0XL3zrW3m+09AMA52NmQV4O1GuyuVkiSVowFb1KfJOAvLjvxtOVJBfPVYr4miTuYtNyu5J2NualfQgp6kr9T24rlPL7N7SmlE5tFCCEEEiVxGG1jorYUCMHiNaiGFwuYDpTnQg3A9ksB/eeRiDTbcfTF1auJUzTKozTleBYyWjU5nE0zYo696xBn8P1RBX18c3LxuJGCBpEUehx5UQW76ySpc0ZfvDaPiUkKXCsdZencplC4e9G15BS9pPy7b6q6kvmO9PSGYa2uWLnCB90ipOE6NZTPdWRyKjgy9O1vY+5zkQsCvt+APU51x/Z8bYCVkbto9Cd3lU/Cs2AIWETX3HI2/uEixyBRLzTsPUoz7z6PXWamFPjNH82jKS8/DpaUorHB9SnoMPsb9G5ntWCAdv7igCrHtyA9vvsHlhg0t10Y5/37ZW3/bMjlnOjH6DHrfDzxS0= kanobi@LAPTOP-8LVA9EO9"
   description = "Public ssh-key"
 
 }
@@ -18,5 +18,15 @@ variable "access_key" {
   type        = string
   description = "My access AWS key"
 
+}
+
+variable "private_ssh_key" {
+  type        = string
+  description = "Private SSH key for execute commands"
+}
+
+variable "devs" {
+  type    = list(map(string))
+  default = [{ login = "bulutovstas", prefix = "balancer" }, { login = "bulutovstas", prefix = "app" }]
 }
 
